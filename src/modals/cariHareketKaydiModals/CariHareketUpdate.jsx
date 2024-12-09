324d11d611c27ef39a2ab96bfb7e0e47c4d35930
Said Erdem Öztürk <saiderdemozturk@gmail.com>
2024-12-09 16:04:02 +0300

    Modals created

---
+++ src/modals/cariHareketKaydiModals/CariHareketUpdate.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import CariIslemlerService from "../../services/cariIslemlerService";
+
+export default function CariHareketUpdate({cariHareket}) {
+    const [date, setDate] = useState("");
+    const [tutar, setTutar] = useState("");
+    const [belgeNo, setBelgeNo] = useState("");
+    const [aciklama, setAciklama] = useState("");
+  
+
+    useEffect(() => {
+        if(cariHareket){
+            setDate(cariHareket.islemTarihi)
+            setTutar(cariHareket.tutar)
+            setBelgeNo(cariHareket.belgeNo)
+            setAciklama(cariHareket.aciklama)
+        console.log("sadasds",cariHareket)
+    
+        }
+    
+      }, [cariHareket])
+  
+
+  const handleDatePicker = (event) => {
+    setDate(event.target.value)
+  };
+
+  const handleTutar = (event) => {
+    setTutar(event.target.value)
+  };
+
+  const handleBelgeNo = (event) => {
+    setBelgeNo(event.target.value)
+  };
+  const handleAciklama = (event) => {
+    setAciklama(event.target.value)
+  };
+
+  const handleKayit = ()=>{
+    const cariIslemlerService = new CariIslemlerService()
+    const cariHareketObject = JSON.parse(cariHareket);
+    cariIslemlerService.cariİslemGuncelle(belgeNo,aciklama,tutar,date,cariHareketObject.fisBaslikId,cariHareketObject.cariId).then((result)=>{
+        cariHareket=null
+        console.log(result)
+    })
+  }
+  const handleYazdir = ()=>{
+
+  }
+
+  const handleSil = ()=>{
+    const cariIslemlerService = new CariIslemlerService()
+    const cariHareketObject = JSON.parse(cariHareket);
+    cariIslemlerService.cariİslemSil(cariHareketObject.hesapId,cariHareketObject.fisBaslikId).then((result)=>{
+        cariHareket=null
+        console.log(result)
+    })
+  }
+  
+
+  return (
+    <div
+      className="modal fade"
+      id="cariHareketUpdateModal"
+      tabIndex="-1"
+      aria-labelledby="cariHareketUpdateModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog ">
+        <div className="modal-content">
+          <div className="modal-header modal-bg">
+            <h5 className="modal-title" id="cariHareketUpdateModalLabel">
+              Nakit - Kredi Kartı - Banka
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body flex-column">
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">İşlem Tarihi</label>
+              <input id="startDate" class="form-control" type="date" value={date} onChange={handleDatePicker}/>
+              
+            </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Belge No</label>
+
+              <input
+                class="form-control"
+                id="inputBelgeNo"
+                aria-describedby="belgeNo"
+                placeholder="Belge No"
+                value={belgeNo}
+                onChange={handleBelgeNo}
+              ></input>
+            </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Açıklama</label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="2"
+                value={aciklama}
+                onChange={handleAciklama}
+              ></textarea>
+            </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Tutar</label>
+              <input type="text" class="form-control" value={tutar} onChange={handleTutar}></input>
+            </div>
+          </div>
+
+          <div className="modal-footer">
+          <button
+            className="btn btn-secondary"
+            type="button"
+            onClick={handleKayit}
+          >
+            Kayıt
+          </button>
+          <button
+            className="btn btn-warning"
+            type="button"
+            onClick={handleYazdir}
+          >
+            Yazdır
+          </button>
+          <button
+            className="btn btn-danger"
+            type="button"
+            onClick={handleSil}
+          >
+            Sil
+          </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/modals/cariHareketKaydiModals/NakitKrediKartiBanka.jsx
+++ src/modals/cariHareketKaydiModals/NakitKrediKartiBanka.jsx
@@ -1,40 +1,151 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import MusteriTedarikciService from "../../services/musteriTedarikciService";
+import { toast } from "react-toastify";
+import { Calendar } from "primereact/calendar";
 
-export default function nakitKrediKartiBanka() {
-    return (
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Nakit - Kredi Kartı -Banka
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+export default function NakitKrediKartiBanka() {
+  const [date, setDate] = useState(null);
+  const [tutar, setTutar] = useState(null)
+  const [type, setType] = useState(null)
+  const [belgeNo, setBelgeNo] = useState(null)
+  const [aciklama, setAciklama] = useState(null)
+  const [hesap, setHesap] = useState(null)
+
+  const handleDatePicker = (event) => {
+    setDate(event.target.value)
+  };
+
+  const handleTutar = (event) => {
+    setTutar(event.target.value)
+  };
+
+  const handleType = (event) => {
+    setType(event.target.value)
+  };
+  const handleBelgeNo = (event) => {
+    setBelgeNo(event.target.value)
+  };
+  const handleAciklama = (event) => {
+    setAciklama(event.target.value)
+  };
+  const handleHesap = (event) => {
+    setHesap(event.target.value)
+  };
+
+  const handleKayit = ()=>{
+    console.log(date,tutar,belgeNo,hesap,aciklama,type)
+  }
+
+
+
+  return (
+    <div
+      className="modal fade"
+      id="nakitKrediKartiBanka"
+      tabIndex="-1"
+      aria-labelledby="nakitKrediKartiBankaLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog ">
+        <div className="modal-content">
+          <div className="modal-header modal-bg">
+            <h5 className="modal-title" id="nakitKrediKartiBankaLabel">
+              Nakit - Kredi Kartı - Banka
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body flex-column">
+            <div className="d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">İşlem Türü</label>
+              <select className="form-select" onChange={handleType}>
+                <option key={0} selected>
+                  Tahsilat
+                </option>
+                <option key={1}>
+                  Tediye/Ödeme
+                </option>
+              </select>
             </div>
-            <div className="modal-body">
-              İşlem Türü
-              Bu bir modal'dır. İçerisine istediğiniz metni veya bileşenleri
-              ekleyebilirsiniz.
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">İşlem Tarihi</label>
+              <input id="startDate" class="form-control" type="date"  onChange={handleDatePicker}/>
+              
             </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Belge No</label>
+
+              <input
+                class="form-control"
+                id="inputBelgeNo"
+                aria-describedby="belgeNo"
+                placeholder="Belge No"
+                onChange={handleBelgeNo}
+              ></input>
+            </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Açıklama</label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="2"
+                onChange={handleAciklama}
+              ></textarea>
+            </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Kasa/Hesap</label>
+              <select
+                className="form-select"
+                aria-label="Gönderilecek hesap seçiniz"
+                onChange={handleHesap}
               >
-                Kapat
-              </button>
-              <button type="button" className="btn btn-primary">
-                Kaydet
-              </button>
+                <option selected>Gönderilecek hesabı seçiniz</option>
+
+                <optgroup label="KASA HESAPLARI">
+                  <option value="kasa_tl">Kasa - TL</option>
+                </optgroup>
+
+                <optgroup label="BANKA HESAPLARI">
+                  <option value="banka_tl">Banka Hesabı - TL</option>
+                  <option value="banka_usd">Banka USD Hesabı - USD</option>
+                  <option value="banka_eur">Banka EUR Hesabı - EUR</option>
+                </optgroup>
+
+                <optgroup label="POS HESAPLARI">
+                  <option value="pos_tl">POS Hesabı - TL</option>
+                </optgroup>
+
+                <optgroup label="ŞİRKET ORTAKLARI HESABI ">
+                  <option value="sirket_tl">Şirket Hesabı - TL</option>
+                </optgroup>
+              </select>
             </div>
+
+            <div className=" d-flex align-items-center justify-content-between my-2">
+              <label className="col-4">Tutar</label>
+              <input type="text" class="form-control" onChange={handleTutar}></input>
+            </div>
+          </div>
+
+          <div className="modal-footer">
+          <button
+            className="btn btn-success"
+            type="button"
+            onClick={handleKayit}
+          >
+            Kayıt
+          </button>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 }
